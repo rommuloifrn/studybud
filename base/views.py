a87a3d8485a5f9692d530d6a0475768dee85f11e
romrom <roomulo.cavalkant@outlook.com>
2022-09-22 21:06:48 -0300

    third

--- base/views.py
+++ base/views.py
@@ -1,22 +1,26 @@
 from django.shortcuts import render
+from .models import Room
 
-rooms = [
-    {'id':1, 'name':'Lets learn python!'},
-    {'id':2, 'name':'Design with me!!'},
-    {'id':3, 'name':'Back end slaves!'},
-]
+#rooms = [
+#    {'id':1, 'name':'Lets learn python!'},
+#    {'id':2, 'name':'Design with me!!'},
+#    {'id':3, 'name':'Back end slaves!'},
+#
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
+    room = Room.objects.get(id=pk)
+
+    #room = None
 
     # O loop for abaixo basicamente confere qual índex de rooms é o pedido na URL.
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    #for i in rooms:
+    #    if i['id'] == int(pk):
+    #        room = i
     context = {'room':room}
     return render(request, 'base/room.html', context)
