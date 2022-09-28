e0e720ebbe50ee3107a5375d1eb6761062e5a0f5
romrom <roomulo.cavalkant@outlook.com>
2022-09-27 23:10:40 -0300

    protoprotótipo

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
 
 #rooms = [
 #    {'id':1, 'name':'Lets learn python!'},
@@ -24,3 +25,16 @@ def room(request, pk):
     #        room = i
     context = {'room':room}
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+
+    context = {'form':form}
+    return render(request, 'base/room_form.html', context)
